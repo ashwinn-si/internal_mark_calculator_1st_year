45a2f374692725e9f93b6f958e1a38ce47c9dd04
ashwinn-si <ashwin23cse@gmail.com>
2024-06-02 21:24:29 +0530

    few_changes_3

--- index.js
+++ index.js
@@ -10,30 +10,36 @@ function main() { // main fucntion
     let internal_mark=internal_mark_calculation(m1_mark,m2_mark,m3_mark);
     internal_mark=extra_criteria(internal_mark)
     let external_mark=external_mark_calculation(internal_mark);
-    display_result(internal_mark,external_mark);//displaying the output
-    
+    display_result(internal_mark,external_mark);
 };
 function exception_condition_checker(m1_mark,m2_mark,m3_mark){ // checks for double clicks or some eror inputs
     if ((skill_done.checked && skill_not_done.checked) || 
         (extra_done.checked && extra_not_done.checked) || 
         (m1_mark > 100 || m2_mark > 100 || m3_mark > 100)) {
-        alert("Wrong input try again !!!");
-        return;
+        return true;
     }
+}
 
-    let internal_mark = Math.floor((((m1_mark * 1.5) + (m2_mark * 1.5)) * 0.075) + ((m3_mark * 1.5) * 0.15));
-    if(internal_mark>=30){
+function internal_mark_calculation(m1_mark,m2_mark,m3_mark){
+    let internal_mark = Math.floor((((m1_mark * 1.5) + (m2_mark * 1.5)) * 0.075) + ((m3_mark * 1.5) * 0.15)); 
+    if(internal_mark>=30){// one more condition checker to avoid errors
         internal_mark=30;
     }
     if (m1_mark + m2_mark + m3_mark >= 120) {
         internal_mark += 5;
     }
+    return internal_mark;
+}
+function extra_criteria(internal_mark){ // checks for extra criteria
     if (skill_done.checked) {
         internal_mark += 2.5;
     }
     if (extra_done.checked) {
         internal_mark += 2.5;
     }
+    return internal_mark
+}
+function external_mark_calculation(internal_mark){ //calcualtes the external mark
     let external_mark = 91;
     if (internal_mark >= 23) {
         external_mark = 45;
@@ -51,6 +57,10 @@ function display_result(internal_mark,external_mark){
     internal_display.classList.add("internal_mark_publisher");
     external_display.classList.add("external_mark_publisher");
     
-    internal_display.innerHTML = "Internal Mark: " + internal_mark;
-    external_display.innerHTML = "External Mark: " + external_mark + " (To pass)";
+    internal_display.innerHTML = "Internal Mark : " + internal_mark;
+    external_display.innerHTML = "External Mark : " + external_mark + " (To pass)";
+    document.querySelector(".cr_1").innerHTML="";
+    document.querySelector(".cr_2").innerHTML="";
+    document.querySelector(".cr_3").innerHTML="CREATED BY : ASHWIN SI 1ST YEAR (CSE-A)";
+    document.querySelector(".cr_4").innerHTML="insta : _ashwin_si";
 }
