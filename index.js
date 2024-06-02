b47d499e8d317d13e82d7e98f22ee4289d213c2b
ashwinn-si <ashwin23cse@gmail.com>
2024-06-02 08:49:56 +0530

    few_changes_2

--- index.js
+++ index.js
@@ -1,35 +1,50 @@
-document.getElementById("calculate").onclick = function() {
-    let m1_mark = parseFloat(document.getElementById("m1_mark").value);
-    let m2_mark = parseFloat(document.getElementById("m2_mark").value);
-    let m3_mark = parseFloat(document.getElementById("m3_mark").value);
-
-
+function main() { // main fucntion 
+    let m1_mark = Number(document.getElementById("m1_mark").value);
+    let m2_mark = Number(document.getElementById("m2_mark").value);
+    let m3_mark = Number(document.getElementById("m3_mark").value);
+    console.log("hi")
+    if(exception_condition_checker(m1_mark,m2_mark,m3_mark)){
+        alert("Wrong input try again !!!");//displays wrong input
+        return;
+    }
+    let internal_mark=internal_mark_calculation(m1_mark,m2_mark,m3_mark);
+    internal_mark=extra_criteria(internal_mark)
+    let external_mark=external_mark_calculation(internal_mark);
+    alert("YOU MUST SCORE " + external_mark + " IN EXTERNAL EXAM TO PASS OUT OF 100\nINTERNAL MARK : "+internal_mark+" OUTOFF 40");//displaying the output
+};
+function exception_condition_checker(m1_mark,m2_mark,m3_mark){ // checks for double clicks or some eror inputs
     if ((skill_done.checked && skill_not_done.checked) || 
         (extra_done.checked && extra_not_done.checked) || 
         (m1_mark > 100 || m2_mark > 100 || m3_mark > 100)) {
-        alert("Wrong input try again !!!");
-        return;
+        return true;
     }
+}
 
-    let internal_mark = Math.floor((((m1_mark * 1.5) + (m2_mark * 1.5)) * 0.075) + ((m3_mark * 1.5) * 0.15));
-    if(internal_mark>=30){
+function internal_mark_calculation(m1_mark,m2_mark,m3_mark){
+    let internal_mark = Math.floor((((m1_mark * 1.5) + (m2_mark * 1.5)) * 0.075) + ((m3_mark * 1.5) * 0.15)); 
+    if(internal_mark>=30){// one more condition checker to avoid errors
         internal_mark=30;
     }
     if (m1_mark + m2_mark + m3_mark >= 120) {
         internal_mark += 5;
     }
+    return internal_mark;
+}
+function extra_criteria(internal_mark){ // checks for extra criteria
     if (skill_done.checked) {
         internal_mark += 2.5;
     }
     if (extra_done.checked) {
         internal_mark += 2.5;
     }
+    return internal_mark
+}
+function external_mark_calculation(internal_mark){ //calcualtes the external mark
     let external_mark = 91;
     if (internal_mark >= 23) {
         external_mark = 45;
     } else {
         external_mark = external_mark - (internal_mark * 2);
     }
-
-    alert("YOU MUST SCORE " + external_mark + " IN EXTERNAL EXAM TO PASS OUT OF 100\nINTERNAL MARK : "+internal_mark+" OUTOFF 40");
-};
+    return external_mark;
+}
